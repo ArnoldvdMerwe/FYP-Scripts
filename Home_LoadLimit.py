d59c67047675954dc85a4f21a0bc0d650b94cd62
Arnold van der Merwe <arnold1.vandermerwe@gmail.com>
2023-10-05 22:21:27 +0200

    Created Python script for home load control
    Created a script that controls the relays of the home platform.
    It uses MQTT to switch relays according to the received message.
    
    Signed-off-by: Arnold van der Merwe <arnold1.vandermerwe@gmail.com>

---
+++ Home_LoadLimit.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import paho.mqtt.client as paho
+import sys
+
+# Setup
+home_num = 1
+gpio_pins = {13: False, 15: False}
+GPIO.setmode(GPIO.BOARD)
+for pin in gpio_pins:
+    GPIO.setup(pin, GPIO.out)
+
+
+def on_message(client, userdata, msg):
+    # Control relays to reduce power use of home as needed
+    received_msg = msg.payload.decode()
+
+    # If msg is activate then reduce load by switching relays
+    if received_msg == "activate":
+        set_pin = False
+        for pin in gpio_pins:
+            if not gpio_pins[pin] and not set_pin:
+                gpio_pins[pin] = True
+                GPIO.output(pin, GPIO.HIGH)
+                set_pin = True
+
+    # If msg is deactivate then stop all load control
+    elif received_msg == "deactivate":
+        for pin in gpio_pins:
+            gpio_pins[pin] = False
+            GPIO.output(pin, GPIO.LOW)
+
+
+client = paho.Client()
+client.on_message = on_message
+
+if client.connect("localhost", 1883, 60) != 0:
+    print("Could not connect to MQTT broker!")
+    sys.exit(-1)
+
+client.subscribe(f"homes/home-{home_num}/load", qos=2)
+
+try:
+    print("Press CTRL+C to exit...")
+    client.loop_forever()
+except:
+    print("Disconnecting from broker!")
+
+client.disconnect()
+GPIO.cleanup()
